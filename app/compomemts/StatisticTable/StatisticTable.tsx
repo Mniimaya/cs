9bd5a259c693c6adddc3dc2371b31678bb57ef64
Mnimaya <mnimayaaa@gmail.com~>
2025-04-26 23:45:11 +0300

    fix: mobile styles

--- app/compomemts/StatisticTable/StatisticTable.tsx
+++ app/compomemts/StatisticTable/StatisticTable.tsx
@@ -26,23 +26,25 @@ export default function StatisticTable({ data }: { data: MapPerformance[] }) {
           <div className={s.header}>
             <h2 className={s.title}>Подробная статистика</h2>
           </div>
-          <div className={s.tableHeader}>
-            <div className={`${s.headerCell} ${s.cardName}`}>Карта</div>
-            <div className={s.headerGroup}>
-              <div className={`{${s.headerCell} ${s.cardNick}`} style={{ textAlign: 'start' }}>
-                Никнейм
+          <div className={s.innerWrapper}>
+            <div className={s.tableHeader}>
+              <div className={`${s.headerCell} ${s.cardName}`}>Карта</div>
+              <div className={s.headerGroup}>
+                <div className={`{${s.headerCell} ${s.cardNick}`} style={{ textAlign: 'start' }}>
+                  Никнейм
+                </div>
+                <div className={s.headerCell}>Кол-во игр</div>
+                <div className={s.headerCell}>Винрейт</div>
+                <div className={s.headerCell}>ADR</div>
+                <div className={s.headerCell}>K/D</div>
               </div>
-              <div className={s.headerCell}>Кол-во игр</div>
-              <div className={s.headerCell}>Винрейт</div>
-              <div className={s.headerCell}>ADR</div>
-              <div className={s.headerCell}>K/D</div>
             </div>
-          </div>
 
-          <div className={s.list}>
-            {data.map((row, index) => (
-              <StatisticsRow data={row} key={index} />
-            ))}
+            <div className={s.list}>
+              {data.map((row, index) => (
+                <StatisticsRow data={row} key={index} />
+              ))}
+            </div>
           </div>
         </div>
       )}
